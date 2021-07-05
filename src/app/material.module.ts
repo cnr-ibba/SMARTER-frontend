bb334cd71452294e17502a4a59c811a81c911ce0
Paolo Cozzi <bunop@libero.it>
2021-07-05 16:01:50 +0200

    :sparkles: add navigation items

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -5,6 +5,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 
 
 @NgModule({
@@ -13,12 +14,14 @@ import { MatButtonModule } from '@angular/material/button';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    MatListModule,
   ],
   exports: [
     MatSidenavModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    MatListModule,
   ],
 })
 export class MaterialModule { }
