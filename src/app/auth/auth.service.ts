d3fb38e3e562a8cf5ac63ab58545b7bf1fb2473b
Paolo Cozzi <bunop@libero.it>
2021-07-07 19:29:31 +0200

    :sparkles: process token after authentication

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,25 +1,37 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { ReplaySubject } from 'rxjs';
+
 import { AuthData } from './auth-data.model';
+import { User } from './user.model';
 
 // declared here and not exported: I don't need it outside this module
 export interface AuthResponseData {
   token: string;
+  expires: string;
 }
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  // "Replays" or emits old values to new subscribers
+  user = new ReplaySubject<User>();
 
   constructor(private http: HttpClient) { }
 
   login(authData: AuthData) {
     this.http.post<AuthResponseData>('http://localhost:27080/api/auth/login', authData)
-      .subscribe(authResponseData => {
-        console.log(authResponseData.token);
-      })
+      .subscribe(
+        authResponseData => {
+          const user = new User(authData.username, authResponseData.token);
+
+          // emit user as a currently logged user
+          this.user.next(user);
+        }, error => {
+          console.log(error);
+        })
   }
 
 }
---
+++ src/app/auth/user.model.ts
@@ -0,0 +1,34 @@
+
+import jwt_decode, { JwtPayload } from 'jwt-decode'
+
+export class User {
+  private _tokenExpirationDate: Date | undefined;
+
+  // constructor enable the new keyword to create an object
+  constructor(
+    public username: string,
+    /* _ and private: I don't want to access the token from outside, if I need the
+    Token, I will call a method that will also check the validity before returning
+    a token string */
+    private _token: string
+  ) {
+    // determining expiration time from token
+    // https://github.com/auth0/jwt-decode/issues/82#issuecomment-782941154
+    const decoded = jwt_decode<JwtPayload>(_token);
+    this._tokenExpirationDate = new Date(Number(decoded.exp) * 1000);
+  }
+
+  // getter: is like a function but is accessed as a property (ex user.token)
+  // code will be executed when accessing this property. Assign a value to
+  // token will throw and error: we need a setter method to set this property
+  get token() {
+    // if i don't have an expiration date or this is less that current time (new Date())
+    if (!this._tokenExpirationDate || new Date() > this._tokenExpirationDate) {
+      // the token is expired, return null even if I have a token
+      return null
+    }
+
+    // return value of a private string
+    return this._token;
+  }
+}
