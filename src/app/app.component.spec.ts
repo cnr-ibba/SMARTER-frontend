c1c6d83bb43bf5bf080eec71705d1036222bd786
Paolo Cozzi <bunop@libero.it>
2021-07-16 15:13:03 +0200

    :white_check_mark: update tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,27 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModule } from './material/material.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule.withRoutes([]),
+        BrowserAnimationsModule,
+        HttpClientTestingModule,
+        MaterialModule,
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        SidenavListComponent,
       ],
     }).compileComponents();
   });
@@ -26,10 +38,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('SMARTER-frontend');
   });
 
-  it('should render title', () => {
+  it('should have router-outlet', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('SMARTER-frontend app is running!');
+    expect(fixture.nativeElement.querySelector('router-outlet')).not.toBeNull();
   });
 });
