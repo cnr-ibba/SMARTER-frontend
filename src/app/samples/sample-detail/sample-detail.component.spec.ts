3ffbe8e9613f0ba589d57af90b19ee7fe96eeeb1
Paolo Cozzi <bunop@libero.it>
2022-10-17 16:26:17 +0200

    :recycle: refactor sample-detail tests

--- src/app/samples/sample-detail/sample-detail.component.spec.ts
+++ src/app/samples/sample-detail/sample-detail.component.spec.ts
@@ -1,10 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-
 import { SampleDetailComponent } from './sample-detail.component';
+import { Sample } from '../samples.model';
+
+const sample: Sample = {
+  "_id": {
+    "$oid": "608ab4b191a0d06725bc0938"
+  },
+  "original_id": "sheep1",
+  "smarter_id": "ITOA-TEX-000000001",
+  "country": "Italy",
+  "species": "Ovis aries",
+  "breed": "Texel",
+  "breed_code": "TEX",
+  "dataset_id": {
+    "$oid": "604f75a61a08c53cebd09b58"
+  },
+  "chip_name": "IlluminaOvineSNP50",
+  "alias": "sheep-one",
+  "type": "background",
+  "phenotype": {},
+  "metadata": {
+    "biosample_id": "ABCDEF",
+    "biosample_url": "http://test.com/ABCDEF"
+  }
+};
+
+const route = { data: of({ sample: sample }) };
 
 describe('SampleDetailComponent', () => {
   let component: SampleDetailComponent;
@@ -18,6 +45,10 @@ describe('SampleDetailComponent', () => {
       ],
       declarations: [ SampleDetailComponent ],
       schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+      // https://stackoverflow.com/questions/60222623/angular-test-how-to-mock-activatedroute-data
+      providers: [
+        { provide: ActivatedRoute, useValue: route },
+      ]
     })
     .compileComponents();
 
@@ -28,5 +59,12 @@ describe('SampleDetailComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('ngOnInit', () => {
+    it('should get sample data', () => {
+      component.ngOnInit();
+      expect(component.sample).toEqual(sample);
+    });
   });
 });
