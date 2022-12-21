3ef2e6ee12562732211460d1bc068ca02a590cc2
Paolo Cozzi <bunop@libero.it>
2022-12-21 16:20:16 +0100

    :recycle: add title to pages

--- src/app/variants/variants.component.spec.ts
+++ src/app/variants/variants.component.spec.ts
@@ -2,6 +2,7 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 
@@ -17,6 +18,7 @@ describe('VariantsComponent', () => {
       imports: [
         BrowserAnimationsModule,
         HttpClientTestingModule,
+        ReactiveFormsModule,
         RouterTestingModule,
         MaterialModule,
       ],
