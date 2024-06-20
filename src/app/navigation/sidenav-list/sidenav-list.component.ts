94215f57649b02a3cade81a860b4bd8684ea9dea
Paolo Cozzi <bunop@libero.it>
2024-06-20 15:26:57 +0200

    :passport_control: remove auth guard

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,29 +1,18 @@
-import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 import { Subscription } from 'rxjs';
-
-import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.scss']
 })
-export class SidenavListComponent implements OnInit, OnDestroy {
+export class SidenavListComponent {
   @Output() closeSidenav = new EventEmitter<void>();
 
   // mind authentication
   isAuthenticated = false;
   authSubscription!: Subscription;
-
-  constructor(private authService: AuthService) { }
-
-  ngOnInit(): void {
-    this.authSubscription = this.authService.user.subscribe(user => {
-      // if I have a user, I'm authenticated
-      this.isAuthenticated = !user ? false : true;
-    });
-  }
 
   onClose() {
     this.closeSidenav.emit();
@@ -32,13 +21,6 @@ export class SidenavListComponent implements OnInit, OnDestroy {
   onLogout() {
     // required to close the sidebar
     this.onClose();
-    this.authService.logout();
-  }
-
-  ngOnDestroy() {
-    if (this.authSubscription) {
-      this.authSubscription.unsubscribe();
-    }
   }
 
 }
